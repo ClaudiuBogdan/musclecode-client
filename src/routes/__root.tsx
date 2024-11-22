b19afa17a525e6e59202ac4298ab159fd9679652
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-11-22 09:38:22 +0200

    feat: add side bar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,21 @@
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { Separator } from "@radix-ui/react-separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const queryClient = new QueryClient();
 
@@ -8,17 +23,22 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light" storageKey="ui-theme">
-        <header>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/algorithm/$id" params={{ id: "1234" }}>Algorithm</Link>
-          </nav>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </ThemeProvider>
+        <ThemeProvider defaultTheme="light" storageKey="ui-theme">
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator orientation="vertical" className="mr-2 h-4" />
+                </div>
+              </header>
+              <main>
+                <Outlet />
+              </main>
+            </SidebarInset>
+          </SidebarProvider>
+        </ThemeProvider>
       </QueryClientProvider>
     </>
   ),
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,16 @@ export default {
           4: "hsl(var(--chart-4))",
           5: "hsl(var(--chart-5))",
         },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       animation: {
         "pulse-attention":
@@ -62,13 +72,13 @@ export default {
         "pulse-attention": {
           "0%, 100%": {
             transform: "scale(1)",
-            opacity: 1,
-            color: "rgb(248 113 113)", // text-red-400
+            opacity: "1",
+            color: "rgb(248 113 113)",
           },
           "50%": {
             transform: "scale(1.05)",
-            opacity: 0.85,
-            color: "rgb(239 68 68)", // text-red-500
+            opacity: "0.85",
+            color: "rgb(239 68 68)",
           },
         },
       },
