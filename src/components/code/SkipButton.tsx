9de658f72a6bfcf7c5f436267d134c2f70e622a3
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-11-28 19:04:50 +0200

    polish: skip button

--- src/components/code/SkipButton.tsx
+++ src/components/code/SkipButton.tsx
@@ -18,12 +18,20 @@ export function SkipButton({ onClick, disabled, className }: SkipButtonProps) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const handleSkip = () => {
-    onClick?.();
-    setDropdownOpen(false);
+    if (!disabled) {
+      onClick?.();
+      setDropdownOpen(false);
+    }
+  };
+
+  const handleOpenChange = (open: boolean) => {
+    if (!disabled) {
+      setDropdownOpen(open);
+    }
   };
 
   return (
-    <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+    <DropdownMenu open={dropdownOpen} onOpenChange={handleOpenChange}>
       <DropdownMenuTrigger asChild>
         <Button
           disabled={disabled}
@@ -32,7 +40,7 @@ export function SkipButton({ onClick, disabled, className }: SkipButtonProps) {
             ${className}
             ${
               disabled
-                ? "text-gray-500 border-gray-800"
+                ? "text-gray-500 border-gray-700 bg-gray-800/50 cursor-not-allowed"
                 : "text-yellow-400 hover:text-yellow-300 border-yellow-800/40 hover:border-yellow-700 bg-yellow-900/10 hover:bg-yellow-900/20"
             }
           `}
@@ -42,30 +50,32 @@ export function SkipButton({ onClick, disabled, className }: SkipButtonProps) {
           Skip
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className="w-56 bg-gray-800 border-gray-700 text-gray-200"
-        align="end"
-        alignOffset={-4}
-      >
-        <div className="px-2 py-2 text-sm text-gray-300">
-          Are you sure you want to skip this problem?
-        </div>
-        <div className="flex border-t border-gray-700">
-          <DropdownMenuItem
-            className="flex-1 justify-center text-gray-300 hover:!text-gray-200 cursor-pointer"
-            onClick={() => setDropdownOpen(false)}
-          >
-            No
-          </DropdownMenuItem>
-          <div className="w-px bg-gray-700" />
-          <DropdownMenuItem
-            className="flex-1 justify-center text-yellow-500 hover:!text-yellow-400 cursor-pointer"
-            onClick={handleSkip}
-          >
-            Yes
-          </DropdownMenuItem>
-        </div>
-      </DropdownMenuContent>
+      {!disabled && (
+        <DropdownMenuContent
+          className="w-56 bg-gray-800 border-gray-700 text-gray-200"
+          align="end"
+          alignOffset={-4}
+        >
+          <div className="px-2 py-2 text-sm text-gray-300">
+            Are you sure you want to skip this problem?
+          </div>
+          <div className="flex border-t border-gray-700">
+            <DropdownMenuItem
+              className="flex-1 justify-center text-gray-300 hover:!bg-gray-700 hover:!text-gray-200 cursor-pointer"
+              onClick={() => setDropdownOpen(false)}
+            >
+              No
+            </DropdownMenuItem>
+            <div className="w-px bg-gray-700" />
+            <DropdownMenuItem
+              className="flex-1 justify-center text-yellow-500 hover:!bg-gray-700 hover:!text-yellow-400 cursor-pointer"
+              onClick={handleSkip}
+            >
+              Yes
+            </DropdownMenuItem>
+          </div>
+        </DropdownMenuContent>
+      )}
     </DropdownMenu>
   );
 }
