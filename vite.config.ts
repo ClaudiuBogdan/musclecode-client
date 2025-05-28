92fb57bab6f0818ed4f209d3d13a158b8f1b8d85
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2025-05-28 08:52:16 +0300

    chore: update .gitignore and comment out ESLint plugin in vite.config.ts
    - Add .eslintcache to .gitignore to prevent caching issues.
    - Comment out the ESLint plugin configuration in vite.config.ts for potential future use.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import eslint from 'vite-plugin-eslint2';
+// import eslint from 'vite-plugin-eslint2';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,12 +13,12 @@ export default defineConfig({
     checker({
       typescript: true,
     }),
-    eslint({
-      // Optional: configure the plugin
-      // cache: false, // Disable cache (default: true)
-      fix: true, // Enable auto-fixing (default: false)
-      include: ['src/**/*.ts', 'src/**/*.tsx'],
-    }),
+    // eslint({
+    //   // Optional: configure the plugin
+    //   // cache: false, // Disable cache (default: true)
+    //   fix: true, // Enable auto-fixing (default: false)
+    //   include: ['src/**/*.ts', 'src/**/*.tsx'],
+    // }),
   ],
   resolve: {
     alias: {
